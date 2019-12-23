0430cc2169013259904286b3fbf91b2cf53ad491
sukhiboi <41sukhdevsingh@gmail.com>
2019-12-23 15:32:34 +0530

    Updated unit tests

--- test/testHeadLib.js
+++ test/testHeadLib.js
@@ -28,41 +28,48 @@ describe("#loadContent()", () => {
     const reader = function(filename, encoding) {
       assert.strictEqual(filename, "appTests/more_than_10_lines.txt");
       assert.strictEqual(encoding, "utf8");
-      return "dummy text";
+      return "some lines of text";
     };
     const filenames = ["appTests/more_than_10_lines.txt"];
     const actual = loadContent(reader, filenames);
-    const expected = "dummy text";
+    const expected = "some lines of text";
     assert.strictEqual(actual, expected);
   });
 
   it("should throw error if file doesn't exists", () => {
     const reader = function(filename, encoding) {
-      assert.strictEqual(filename, "dummy.txt");
+      assert.strictEqual(filename, "invalid_file.txt");
       assert.strictEqual(encoding, "utf8");
-      return "dummy text";
+      return "invalid_file";
     };
     const actual = function() {
       loadContent(reader, filenames);
     };
-    const expected = "head: dummy.txt: No such file or directory";
-    const filenames = ["dummy.txt"];
+    const expected = "head: invalid_file.txt: No such file or directory";
+    const filenames = ["invalid_file.txt"];
     assert.throws(actual, expected);
   });
 });
 
 describe("#filterLines()", () => {
-  it("should give first 10 lines when -n is not specified", () => {
+  it("should give first 10 lines when there are more than 10 lines in content", () => {
     const content = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11";
     const actual = filterLines(content, 10);
     const expected = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
     assert.strictEqual(actual, expected);
   });
 
-  it("should give all the content, when content has less than 10 lines", () => {
-    const content = "1\n2\n3\n4\n5\n6";
+  it("should give first 10 lines when there are only 10 lines in content", () => {
+    const content = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
     const actual = filterLines(content, 10);
-    const expected = "1\n2\n3\n4\n5\n6";
+    const expected = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
+    assert.strictEqual(actual, expected);
+  });
+
+  it("should give all the when there less than ten lines in content", () => {
+    const content = "1\n2\n3\n4\n5\n6\n7";
+    const actual = filterLines(content, 10);
+    const expected = "1\n2\n3\n4\n5\n6\n7";
     assert.strictEqual(actual, expected);
   });
 
