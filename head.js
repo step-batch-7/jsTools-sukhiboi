34da26d50a2b61b32cc954d87886315ce97861be
sukhiboi <41sukhdevsingh@gmail.com>
2020-01-02 10:15:48 +0530

    Updated main function
    What changes you have made?
    - Pull out the callback function of filterHeadLines to a seprate
    function.
    
    Why this change is necessary?
    - To make code simpler.
    
    What is the impact of the change?
    - Code is more simpler and readable.

--- head.js
+++ head.js
@@ -5,10 +5,13 @@ const { filterHeadLines, getInputStream } = require('./src/headLib');
 const main = function () {
   const [, , filename] = process.argv;
   const inputStream = getInputStream(filename, fs.createReadStream, stdin);
-  filterHeadLines(inputStream, headOutcome => {
-    stderr.write(headOutcome.errMsg);
-    stdout.write(headOutcome.headLines);
-  });
+  
+  const logger = function (headOutCome) {
+    stderr.write(headOutCome.errMsg);
+    stdout.write(headOutCome.headLines);
+  };
+
+  filterHeadLines(inputStream, logger);
 };
 
 main();
